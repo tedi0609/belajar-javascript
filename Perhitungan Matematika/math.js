6054331f3ed35813aeb2a283b20f4e091c0dd450
tedi0609 <tedysahputra692@gmail.com>
2022-04-19 12:22:16 +0800

    objek math

---
+++ Perhitungan Matematika/math.js
@@ -0,0 +1,45 @@
+Math.pow(2, 4)
+16
+math.pow(4, 6)
+
+math.pow(4, 6)
+
+Math.pow(4, 6)
+4096
+Math.exp(1)
+2.718281828459045
+Math.exp(3)
+20.085536923187668
+Math.floor(2.4)
+2
+Math(2.7)
+
+Math.floor(2.7)
+2
+
+Math.ceil(2.4)
+3
+Math.round(2.6)
+3
+Math.sqrt(40)
+6.324555320336759
+Math.cbrt(125)
+5
+Math.pow(625, 1 / 4)
+5
+Math.random()
+0.9739389319720189
+Math.random()
+0.004172115603662041
+function getRndInteger(min, max) {
+   return Math.floor(Math.random() * (max - min)) + min;
+}
+undefined
+getRndInteger(1, 100)
+45
+Math.min(2, 3, 5, 6, 7)
+2
+Math.max(3, 5, 7, 675);
+675
+Math.max(...[32, 24, 11, 56])
+56
